d767fcdee7d990a3609972f4f873248c5986ab70
Aditya Pant <aditya@vakeelsaab.com>
2026-02-04 15:48:36 +0530

    removed News section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -12,7 +12,7 @@ export default function Home() {
       <Hero />
       {/* <DistinguishedAlumniSection /> */}
       <UpcomingEvents />
-      <LatestNews />
+      {/* <LatestNews /> */}
       {/* <CallToAction /> */}
     </>
   );
